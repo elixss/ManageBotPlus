b7ef0e377df2ea227f769b4d5c2175757e13ab3a
elias <elias@ELIXSS-PC>
2022-11-24 22:20:13 +0100

    edited last update

--- ManageBotPlus/Config/Config.cs
+++ ManageBotPlus/Config/Config.cs
@@ -4,7 +4,7 @@ namespace ManageBotPlus
 {
     public struct Config
     {
-        public static int LastUpdate => 1669144669;
+        public static int LastUpdate => 1669324688;
         public static Color Color => 0xac45ec;
         public static string Developer => "elixss#9999";
         public static ulong TestGuildId => 890279450376294453;
