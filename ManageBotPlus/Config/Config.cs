7c56fba6078be6795e3f404bcb72f0c5b9a64f8b
elias <elias@ELIXSS-PC>
2022-11-26 00:10:55 +0100

    Changed config to readonly struct, all values to fields + made them static and readonly and added field for modules and module count.

--- ManageBotPlus/Config/Config.cs
+++ ManageBotPlus/Config/Config.cs
@@ -1,14 +1,19 @@
 ﻿using Discord;
+using System.Reflection;
 
 namespace ManageBotPlus
 {
-    public struct Config
+    public readonly struct Config
     {
-        public static int LastUpdate => 1669324688;
-        public static Color Color => 0xac45ec;
-        public static string Developer => "elixss#9999";
-        public static ulong TestGuildId => 890279450376294453;
-        public static DateTime StartTime => DateTime.UtcNow;
-        public static string SupportInvite => "https://discord.gg/5mYSubhkrg";
+        public static readonly int lastUpdate = 1669324688;
+        public static readonly Color color = 0xac45ec;
+        public static readonly string developer = "elixss#9999";
+        public static readonly ulong testGuildId = 890279450376294453;
+        public static readonly DateTime startTime = DateTime.UtcNow;
+        public static readonly string supportInvite = "https://discord.gg/5mYSubhkrg";
+        public static readonly List<Type> modules = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => type.IsClass && type.GetInterfaces().Contains(typeof(IManageBotModule)))
+            .ToList();
+        public static readonly int moduleCount = modules.Count;
     }
 }
