3ea785cbc9cbf6751783ab62b840e8fae012e1c3
elias <elias@ELIXSS-PC>
2022-12-31 03:20:09 +0100

    yes i broke the bot i just fixed it lol

--- ManageBotPlus/Program.cs
+++ ManageBotPlus/Program.cs
@@ -54,7 +54,7 @@ namespace ManageBotPlus
                 Console.WriteLine(message);
             };
 
-            //this._client.InteractionCreated += this.CheckForManageBot;
+            this._client.InteractionCreated += this.CheckForManageBot;
             this._interactionService.SlashCommandExecuted += SlashCommandExecutedHandler.SlashCommandExecuted;
 
             this._client.Ready += this.SetupCommandsAsync;
@@ -73,17 +73,17 @@ namespace ManageBotPlus
 
         public async Task CheckForManageBot(SocketInteraction interaction)
         {
-            IGuild guild = this._client.GetGuild((ulong)interaction.GuildId);
-            if (interaction is SocketSlashCommand command)
-            {
-                AllowedGuildsModel guildIds = await JsonUtil.GetJsonAsync<AllowedGuildsModel>("allowedGuilds.json");
-                if ((await guild.GetUserAsync(890273033204420678)) == null && !guildIds.AllowedGuilds.Contains((ulong)interaction.GuildId))
-                {
-                    await command.RespondAsync("It appears, that ManageBot is not on this server. To use this bot, you must [invite ManageBot](https://discord.com/api/oauth2/authorize?client_id=890273033204420678&permissions=1374859684086&scope=bot%20applications.commands). " +
-                        "If you think this is a mistake, please [join the support server](https://discord.gg/5mYSubhkrg) or contact a server moderator.", ephemeral: true);
-                    return;
-                }
-            }
+            //IGuild guild = this._client.GetGuild((ulong)interaction.GuildId);
+            //if (interaction is SocketSlashCommand command)
+            //{
+            //    AllowedGuildsModel guildIds = await JsonUtil.GetJsonAsync<AllowedGuildsModel>("allowedGuilds.json");
+            //    if ((await guild.GetUserAsync(890273033204420678)) == null && !guildIds.AllowedGuilds.Contains((ulong)interaction.GuildId))
+            //    {
+            //        await command.RespondAsync("It appears, that ManageBot is not on this server. To use this bot, you must [invite ManageBot](https://discord.com/api/oauth2/authorize?client_id=890273033204420678&permissions=1374859684086&scope=bot%20applications.commands). " +
+            //            "If you think this is a mistake, please [join the support server](https://discord.gg/5mYSubhkrg) or contact a server moderator.", ephemeral: true);
+            //        return;
+            //    }
+            //}
 
             var ctx = new SocketInteractionContext(this._client, interaction);
 
